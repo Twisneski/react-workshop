c58680304b7f642dde119b64744ce020583c7b11
William Golden <egdelwonk@gmail.com>
2016-06-13 16:58:18 -0500

    enabled editing an item

--- src/components/todo-item.js
+++ src/components/todo-item.js
@@ -1,15 +1,41 @@
 import React, { PropTypes } from 'react'
 
 class TodoItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editing: false
+    };
+  }
   render () {
     let props = this.props;
+
+    let label = (
+      <div className="label" onDoubleClick={() => {
+          this.setState({editing: true});
+        }}>{props.label}</div>
+      );
+
+    if (this.state.editing) {
+        label = <input type="text" defaultValue={props.label} onBlur={(e) => {
+          let text = e.target.value;
+          if (!text) {
+            e.targer.value = props.label;
+          }
+          props.onItemEdit(props.id, text); 
+          this.setState({editing: false});
+        }}/>
+    }
+
     return (
       <div className="todo-item">
         <input type="checkbox" checked={props.completed} onChange={() => {
-          console.log('called itemToggle');
           props.onItemToggle(props.id);
         }}/>
-        <div className="label">{props.label}</div>
+
+        {label}
+
         <button className="delete-btn" onClick={(e) => {
           if (confirm(`Are you sure you want to delete ${props.label}?`)) {
             props.onItemDelete(props.id);
