e9b6b139657df8f77acd64237cc71fad4bc6b02f
William Golden <egdelwonk@gmail.com>
2016-06-13 16:08:19 -0500

    wired up filtering

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -2,10 +2,20 @@ import React from 'react';
 import TodoItem from './todo-item';
 
 const TodoList = (props) => {
+  let todos = props.todos.filter((todo) => {
+      if (props.filter === 'active') {
+        return !todo.completed
+      } else if (props.filter === 'completed') {
+        return todo.completed
+      } else {
+        return true;
+      }
+  });
+
   return (
     <div className="todo-list">
       {
-        props.todos.map((todo) => {
+        todos.map((todo) => {
           return <TodoItem key={todo.id} label={todo.label} />
         })
       }
